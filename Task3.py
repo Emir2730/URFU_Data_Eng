1a9d52f1e9b901838a51438c007df97ed48f9bcc
Emir Culiev <oznobishina22@gmail.com>
2023-11-01 15:05:39 +0500

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -10,9 +10,7 @@ with open(filename) as f:
 
 with open(filename+"out", "w") as f:
     for l in lines:
-        # List comprehension
         tl = [w for w in l.strip().split(det)]
-        # Generator expression
         lq = (int(w) if w.isdigit() else (int(tl[i-1]) + int(tl[i+1])) / 2 for i,w in enumerate(tl))
         [f.write(f"{i},") if i ** 0.5 >= less_v else "" for i in lq]
         f.write("\n")

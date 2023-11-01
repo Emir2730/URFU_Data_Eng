342605a25fb4598eea8ea9e13d21bdcccffb6c16
Emir Culiev <oznobishina22@gmail.com>
2023-11-01 09:09:58 +0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,13 @@
+
+
+filename = "text/text_2_var_63"
+det = ","
+
+with open(filename) as f:
+    lines = f.readlines()
+
+with open(filename + "out", "w") as f:
+    for l in lines:
+        f.write(f"{sum(map(int, l.strip().split(det)))}\n")
+       
+
